547b4cfa409285377b0386376c1fc7fc4ba779a6
SDozH <soldatov_alex@bk.ru>
2022-07-22 09:03:39 +0300

    Добавьте файлы проекта.

---
+++ Unit 9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Unit_9
+{
+   public class Program
+    {
+        delegate int CalculateDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            CalculateDelegate calcDelegate = Calculate;
+            int result = calcDelegate.Invoke(100, 30);
+
+            Console.WriteLine(result);
+            Console.Read();
+
+        }
+        static int Calculate(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
+    
