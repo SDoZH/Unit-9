8cea1a1e328855cbfab5a9f01a73683aa848cc06
SDozH <soldatov_alex@bk.ru>
2022-07-24 22:17:34 +0300

    9/3/12

--- Unit 9/Program.cs
+++ Unit 9/Program.cs
@@ -2,37 +2,18 @@
 
 namespace Unit_9
 {
-   public class Program
-    {     
+    public class Program
+    {
+        delegate void ShowMessageDelegate(string _message);
         static void Main(string[] args)
         {
-            Action showMessageDelegate = ShowMessage;
-            showMessageDelegate.Invoke();
-
-            Func<int, int, int, int> sumDelegate = Sum;
-            int result = sumDelegate.Invoke(1, 30, 120);
-            Console.WriteLine(result);
-
-            Predicate<string> checkLengthDelegate = CheckLength;
-            bool status = checkLengthDelegate.Invoke("skill_factory");
-            Console.WriteLine(status);
-        }
-       public static void ShowMessage()
-        {
-            Console.WriteLine("Hello World!");
-        }
-
-       public static int Sum(int a, int b, int c)
-        {
-            return a + b + c;
-        }
-
-        public static bool CheckLength(string _row)
-        {
-            if (_row.Length > 3) return true;
-            return false;
+            ShowMessageDelegate showMessageDelegate = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
         }
     }
-
 }
-    
+
